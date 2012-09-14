1e97389d16ab62843a03443adf9410ef9fafe97e
Marinus Swanepoel <mswanepoel@Marinuss-MacBook-Pro.local>
2012-09-14 11:17:04 -0400

    about to use tinycss

---
+++ resize_css.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python
+import os
+import fileinput
+
+filePathGlobal = ''
+lineNumber = 0
+selectorsAndRules = None
+currentSelector = None
+currentRule = None 
+selectorMade = False
+selector = ''
+cssProperty = ''
+propertyFinished = False
+			
+def makedir(dirpath):
+	direxists = os.path.isdir(dirpath)
+	#print "Dir exists: %r" % (direxists) 
+	if direxists != True:
+		scssDir = os.mkdir(dirpath) 
+	scssDir = dirpath
+
+def parseit(filepath):
+	print 'opening ' + filepath
+	global lineNumber
+	global filePathGlobal
+	lineNumber = 0
+	filePathGlobal = filepath
+	opfile  = open(filepath)
+	words = opfile.read().replace('\n', ' ').split()
+	for word in words:
+		global selector
+		global selectorMade
+		global propertyFinished
+		if selectorMade == False and word.find('{')!= -1:
+			selector = selector + ' ' + word[:word.index('{')]
+			selectorMade = True
+			print 'we have a selector it is: ' + selector 
+			word = word[word.index('{'):]
+		
+		cssProperty = cssProperty  + word
+		if selectorMade == True:
+			if word.find(':') != -1:
+				cssropety
+			else
+				cssProperty = cssProperty + ' ' + word
+	'''linesArray = opfile.readlines()
+	for line in linesArray:
+		global lineNumber 
+		lineNumber += 1
+		#print line.replace('\n','')
+		analyzeLine(line)	
+	'''
+def getExtention(filepath):
+	#print os.path.join(dirname, filename)
+	fileName, fileExtension = os.path.splitext(filepath)
+	#print fileExtension[1:]
+	if fileExtension[1:] == 'css':
+		#print 'found css file '+fileName
+		parseit(filepath)
+
+scssDir = makedir('scss')
+for dirname, dirnames, filenames in os.walk('.'):
+    for filename in filenames:
+		filepath = os.path.join(dirname, filename)
+		getExtention(filepath)
+        
